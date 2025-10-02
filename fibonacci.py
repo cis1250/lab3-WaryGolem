81848a6cd2e4a5569da48d2200f09b92030647a5
Shayan Saeed <ssaeed19@uoguelph.ca>
2025-10-01 23:45:07 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -2,7 +2,7 @@
 
 # Fibonacci Sequence Exercise
 # TODO: (Read detailed instructions in the Readme file)
-# Prompt the user for the number of terms.
+# Prompt the user for the number of terms.j
 # Validate that the input is a positive integer.
 # Use a for loop to print the Fibonacci sequence up to that many terms.
 While true:
